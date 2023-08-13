43cb7af0382f4dd1c690e1a9739f911d76a51b31
Sharon Atim <venusatim@gmail.com>
2023-08-13 21:48:18 +0100

    repository added

---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_tdd_clean_architecture/core/error/failures.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure,NumberTrivia>> getConcreteNumberTrivia(int number);
+  Future<Either<Future,NumberTrivia>> getRandomNumberTrivia();
+}
